e515d2e6af1faa53e2c0079e19813aef21437c21
amoro99 <andrew@entrinsik.com>
2018-01-15 16:07:08 -0500

    Initial Commit - based in Wynne Systems

--- test/module-spec.js
+++ test/module-spec.js
@@ -2,9 +2,38 @@
 
 var should = require('chai').should();
 var lib = require('../');
+var Moment = require('Moment');
 
-describe('your module', function () {
+
+describe('The db2dates data type interceptor', function () {
+    before(function() {
+        var self = this;
+        //this is called to get us access to the plugin
+        var srvr = {
+            dm: function() {
+                return  {
+                    intercept: function(plugin) {self.plugin = plugin}
+                };
+            }
+        };
+        lib.register(srvr, null, () => {});
+    });
     it('should exist', function () {
         should.exist(lib);
     });
+    it('should parse a date correctly and return a number', function() {
+        var numericValue = this.plugin.parse('date','date',new Moment('2017-02-02').toDate(), ()=>{});
+        numericValue.should.equal(20170202);
+    });
+    it('should convert a date value correctly and return a date', function() {
+        this.plugin.convert('date',20101102,() => null).getTime().should.equal(new Moment('2010-11-02').toDate().getTime());
+    });
+    it('should not attempt to convert a null number', function() {
+        var converted = this.plugin.convert('date',null,() => null);
+        (converted === null).should.be.true;
+    })
+    it('should return null when converting zero', function() {
+        var converted = this.plugin.convert('date',0,() => null);
+        (converted === null).should.be.true;
+    })
 });
